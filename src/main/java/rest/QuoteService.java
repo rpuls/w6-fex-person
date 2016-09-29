946917b6e87d535a319b4cf8fade9c51614a7ee5
rasmus <rasmus@DESKTOP-VP3V2CP>
2016-09-29 12:31:47 +0200

    quotes works

--- src/main/java/rest/QuoteService.java
+++ src/main/java/rest/QuoteService.java
@@ -5,8 +5,13 @@
  */
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entity.Person;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -15,6 +20,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
+import static rest.PersonService.facade;
 
 /**
  * REST Web Service
@@ -23,12 +29,14 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("quote")
 public class QuoteService {
+    
+    static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     private static Map<Integer, String> quotes = new HashMap() {
         {
-            put(1, "RESTful programming is very tough, and code is fragile");
-            put(2, "Do not take life too seriously. You will never get out of it alive");
-            put(3, "Behind every great man, is a woman rolling her eyes");
+            put(1, "RESTful programming is very tough, and the code is fragile");
+            put(2, "Servlet.init() exception is very strange and stupid");
+            put(3, "AJAX is smart and very useful!");
         }
     };
     
@@ -45,11 +53,25 @@ public class QuoteService {
      * Retrieves representation of an instance of rest.QuoteService
      * @return an instance of java.lang.String
      */
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String getQuote() {
+//        String quote;
+//        Random generator = new Random();
+//        quote = quotes.get(1);
+//        //randomValue = entries[generator.nextInt(entries.length)].getValue();
+//        //return gson.toJson(quote);
+//        return "[]";
+//
+//    }
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+        Random generator = new Random();
+        String quote;
+        quote = quotes.get(generator.nextInt(quotes.size())+1);
+        return gson.toJson(quote);
     }
 
     /**
